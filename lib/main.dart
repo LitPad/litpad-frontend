46338679288bf707c21e0f732af84510f438a494
Obiaderi <108232627+Obiaderi@users.noreply.github.com>
2024-03-29 10:51:55 +0100

    Initialize ScreenUtil in MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      // Initialize the ScreenUtil
       designSize: const Size(1440, 1024),
       builder: (context, child) {
         return MaterialApp.router(
