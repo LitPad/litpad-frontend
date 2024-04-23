0c19c18f2cce0f3081cd64f51007f23d4dc505e7
Jeremiah Ekanem <jeremyessien@gmail.com>
2024-04-23 16:46:05 +0100

    [Integrating endpoint] - Initialised env on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 import 'app/locator.dart';
@@ -8,6 +9,8 @@ void main() async {
   setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   setupLocator();
+  await dotenv.load();
+
   // final startupLogic = StartupLogic();
   // await startupLogic.initialize();
   runApp(const MyApp());
